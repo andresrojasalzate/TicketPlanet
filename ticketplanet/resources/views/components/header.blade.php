6fdd723f63a490aad86f05a6396cbb404b5c1a21
Salma <spicazo@alumnat.copernic.cat>
2023-12-15 15:43:40 +0100

    Creación header (no acabado)

--- ticketplanet/resources/views/components/header.blade.php
+++ ticketplanet/resources/views/components/header.blade.php
@@ -1,13 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="icon" href="{{ asset('images/logoFavicon.ico') }}">
-</head>
-<body>
-    
-</body>
-</html>
+<nav>
+    <div class="nav">
+        <img src="{{ asset('images/logo.jpg') }}" alt="Logo" width="100">
+        <div class="links">
+            <ul>
+                <li><a href="{{ route('home') }}">Home</a></li>
+                <li><a href="{{ route('links.aboutus') }}">Sobre nosotros</a></li>
+                <li><a href="{{ route('links.legalnotice') }}">Avisos legales</a></li>
+            </ul>
+        </div>
+        <div class="auth">
+            <img src="{{ asset('images/usuario.png') }}" alt="User" width="50">
+            <a href="">Acceder</a>
+        </div>
+    </div>
+    <div class="navLogo">
+        <img src="{{ asset('images/LogoNombre.png') }}" alt="User" width="400">
+        
+    </div>
+
+</nav>
--- ticketplanet/routes/web.php
+++ ticketplanet/routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LinksController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/links/home');
 });
+
+
+Route::get('/links/home', [LinksController::class, 'home'])->name('home');
+Route::get('/links/aboutus', [LinksController::class, 'aboutUs'])->name('links.aboutus');
+Route::get('/links/legalnotice', [LinksController::class, 'legalNotice'])->name('links.legalnotice');
