c9a18407ea51e5757b35af61d591f1c1849a3f14
Salma <spicazo@alumnat.copernic.cat>
2024-01-08 16:42:50 +0100

    Controlada la cantidad de eventos mostrados

--- ticketplanet/app/Http/Controllers/EventController.php
+++ ticketplanet/app/Http/Controllers/EventController.php
@@ -9,7 +9,8 @@ use App\Models\Event;
 class EventController extends Controller
 {
     public function index() {
-        return view('events.index', ['events' => Event::with('sessions')->get()]);
+        //Controlamos la cantidad de eventos mostrados con el "take", máximo 5
+        return view('events.index', ['events' => Event::with('sessions')->take(5)->get()]);
         //return Event::with('sessions')->get();
 
     }
