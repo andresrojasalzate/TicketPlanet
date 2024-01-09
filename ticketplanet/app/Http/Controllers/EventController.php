31820e958b59b4704a10208aeed0044ad646e0b3
Salma <spicazo@alumnat.copernic.cat>
2024-01-09 16:55:50 +0100

    Modificado el styles.css, vista correcta

--- ticketplanet/app/Http/Controllers/EventController.php
+++ ticketplanet/app/Http/Controllers/EventController.php
@@ -9,8 +9,7 @@ use App\Models\Event;
 class EventController extends Controller
 {
     public function index() {
-        //Controlamos la cantidad de eventos mostrados con el "take", máximo 5
-        return view('events.index', ['events' => Event::with('sessions')->take(5)->get()]);
+        return view('events.index', ['events' => Event::with('sessions')->get()]);
         //return Event::with('sessions')->get();
 
     }
--- ticketplanet/resources/views/events/index.blade.php
+++ ticketplanet/resources/views/events/index.blade.php
@@ -1,68 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="{{ asset('css/styles.css') }}"> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="shortcut icon" href="favicon/logoFavicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
+
 <body>
-  <header>
-    <div class="dropdown">
-      <button class="botonDespegable"></button>
-      <div class="dropdown-content">
-      <p>Home</p>
-      <p>Categoria 1</p>
-      <p>Categoria 2</p>
-      <p>Categoria 3</p>
-      <p>Categoria 4</p>
-      <p>Categoria 5</p>
-      <hr>
-      
-      <p>Sobre nosotros</p>
-      <p>Avisos legales</p>
-
-      <img  class="imagenUsuarioDespegable" src="{{ asset('images/logo.jpg') }}" alt="" width="80">
-      </div>
-    </div>
-    <div class="header">
-    <img src="{{ asset('images/logo.jpg') }}" alt="" height="50">
-
-    <div class="h1">
-      <p>Home</p>
-      <p>Sobre nosotros</p>
-      <p>Avisos Legales</p>
-    </div>
-    <img class="imagenUsuario"  src="{{ asset('images/usuario.png') }}" alt="" width="40">
-    </div>
-    <div class="nombre">
-
-      <img class="imagenNombre" src="{{ asset('images/LogoNombre.png') }}" alt="" width="300">
-
-    </div>
-  </header>
-  <div class="layout">
-    <div class="contenedorBuscador">
-      <img  class="imagenLupa" src="Imagenes/lupa.png" alt="" height="30">
-      <input class="buscador" type="search" placeholder="Buscar">
-      <img  class="ImagenFiltrar" src="Imagenes/Filtrar.png" alt="" height="30">
-    <button class="botonBuscador" type="button">Buscar</button>
-    </div>
-    <div class="event-counter">
-      @foreach  ($events as $event)
-        <div>
-          <x-event-component :event="$event"/>
+    <header>
+        <x-header/>
+    </header>
+    
+    <div class="layout">
+        <div class="contenedorBuscador">
+            <img class="imagenLupa" src="images/buscador/lupa.png" alt="" height="30">
+            <input class="buscador" type="search" placeholder="Buscar">
+            <img class="imagenFiltrar" src="images/buscador/filter.png" alt="" height="30">
+            <button class="botonBuscador" type="button">Buscar</button>
         </div>
-      @endforeach
+        <div class="event-counter">
+            @foreach ($events as $event)
+                <div>
+                    <x-event-component :event="$event" />
+                </div>
+            @endforeach
+        </div>
     </div>
-  </div>
-  
-  <footer>
-    <p class="item-1">Home Promotor</p>
-    <p class="item-2">Sobre nosaltres</p>
-    <p class="item-3">Avisos legales</p>
-    <p class="item-4">Mostrar Esdeveniments</p>
-    <p class="item-5">Comprar entrades</p>
-  </footer>
+
+    <footer>
+        <x-footer/>
+    </footer>
 </body>
+
 </html>
