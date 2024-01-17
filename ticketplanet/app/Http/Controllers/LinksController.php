b620632a2260d1c62fc7f6137bdeb92425483f1a
Andrés Rojas Alzate <arojas@alumnat.copernic.cat>
2024-01-17 15:58:32 +0000

    Andres

--- ticketplanet/app/Http/Controllers/LinksController.php
+++ ticketplanet/app/Http/Controllers/LinksController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Response;
 
 class LinksController extends Controller
 {
@@ -25,6 +28,29 @@ class LinksController extends Controller
     {
       return view('links.crearEvento');
     }
+
+    public function guardarEvento(Request $request)
+    {
+      $nombreLocal = $request->input('nombreLocal');
+
+      if ($request->hasCookie('direccion')) {
+
+
+        Log::info("se ha encotrado la cookie");
+
+        return "cookie existe";
+
+      } else {
+
+        $response = new Response("Cookie no existe");
+        $response->withCookie(Cookie::forever('direccion', $nombreLocal));
+        Log::info("Se crea la cookie"); 
+        return $response;
+
+      }
+
+    }
+
     public function comprarEntradas()
     {
       return view('links.comprarEntradas');
