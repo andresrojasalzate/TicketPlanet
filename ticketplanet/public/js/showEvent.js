32a7127f3f61646677ecd408e050b2d8f04f36b2
Salma <spicazo@alumnat.copernic.cat>
2024-01-19 17:52:35 +0100

    Implementación desplegable sesiones al mostrar la informacion de los eventos

---
+++ ticketplanet/public/js/showEvent.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownSesiones = document.getElementById('dropdownSesiones');
+    const sessionTimes = document.querySelectorAll('.session-time');
+
+    dropdownSesiones.addEventListener('change', function () {
+        const selectedDate = dropdownSesiones.options[dropdownSesiones.selectedIndex].getAttribute('data-date');
+
+        sessionTimes.forEach(sessionTime => {
+            const sessionDate = sessionTime.getAttribute('data-date');
+            sessionTime.style.display = (sessionDate === selectedDate || selectedDate === null) ? 'block' : 'none';
+        });
+    });
+});
--- ticketplanet/resources/views/components/event-component.blade.php
+++ ticketplanet/resources/views/components/event-component.blade.php
@@ -1,13 +1,13 @@
-
 <div class="show-event-home">
     <a href="{{ route('events.mostrar', ['id' => $event->id]) }}">
-    <img class="show-event-home-img" src="{{ $event->image }}" alt="">
+        <img class="show-event-home-img" src="{{ $event->image }}" alt="">
         <p class="show-event-home-title">{{ $event->name }}</p>
         <div class="elementsEvents">
-        <p><img src="images/eventos/location.png" alt="" height="25">{{ $event->site }}</p>
-        <p><img src="images/eventos/calendar.png" alt="" height="25">{{ $event->sessions[0]->date }}</p>
-        <p><img src="images/eventos/precio.png" alt="" height="25">Desde&nbsp;<strong>{{ $event->sessions[0]->price }}€</strong>
-        </p>
-    </div>
-</a>
+            <p><img src="images/eventos/location.png" alt="" height="25">{{ $event->site }}</p>
+            <p><img src="images/eventos/calendar.png" alt="" height="25">{{ $event->sessions[0]->date }}</p>
+            <p><img src="images/eventos/precio.png" alt=""
+                    height="25">Desde&nbsp;<strong>{{ $event->sessions[0]->price }}€</strong>
+            </p>
+        </div>
+    </a>
 </div>
