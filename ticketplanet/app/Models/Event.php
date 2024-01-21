cfa19a564d568f850b93f81cd270bfcfca9286e4
Andrés Rojas Alzate <arojas@alumnat.copernic.cat>
2024-01-21 20:57:00 +0000

    solucionado probleemas del buscador. Y ajustado los compomentenes de mostar...

--- ticketplanet/app/Models/Event.php
+++ ticketplanet/app/Models/Event.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Support\Facades\Log;
 
 class Event extends Model
@@ -20,6 +21,11 @@ class Event extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function tickets(): HasManyThrough
+    {
+        return $this->hasManyThrough(Ticket::class, Session::class);
     }
 
     protected $fillable = [
