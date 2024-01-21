94a35a728f9f47dc7b4f6a0a5292b4c2dfd356a9
Andrés Rojas Alzate <arojas@alumnat.copernic.cat>
2024-01-21 17:05:10 +0000

    solucionado probleemas del buscador. Y ajustado los compomentenes de mostar...

--- ticketplanet/resources/views/components/event-component.blade.php
+++ ticketplanet/resources/views/components/event-component.blade.php
@@ -3,9 +3,9 @@
         <img class="show-event-home-img" src="{{ $event->image }}" alt="">
         <p class="show-event-home-title">{{ $event->name }}</p>
         <div class="elementsEvents">
-            <p><img src="images/eventos/location.png" alt="" height="25">{{ $event->site }}</p>
-            <p><img src="images/eventos/calendar.png" alt="" height="25">{{ $event->sessions[0]->date }}</p>
-            <p><img src="images/eventos/precio.png" alt=""
+            <p><img src="{{ asset('images/eventos/location.png') }}" alt="" height="25">{{ $event->address}}, {{ $event->city}}, {{ $event->name_site}}</p>
+            <p><img src="{{ asset('images/eventos/calendar.png') }}" alt="" height="25">{{ $event->sessions[0]->date }}</p>
+            <p><img src="{{ asset('images/eventos/precio.png') }}" alt=""
                     height="25">Desde&nbsp;<strong>{{ $event->sessions[0]->price }}€</strong>
             </p>
         </div>
--- ticketplanet/resources/views/events/index.blade.php
+++ ticketplanet/resources/views/events/index.blade.php
@@ -5,19 +5,25 @@
 @section('content')
 
 <div class="layout">
-    <x-buscador/>
+    <x-buscador :categories="$categories"/>
+    @if($events != null && !$events->isEmpty())
+        <div class="event-counter">
+            
+            @foreach ($events as $event)
+                <div>             
+                    <x-event-component :event="$event" />        
+                </div>
+            @endforeach
+        </div>
 
-    <div class="event-counter">
-        @foreach ($events as $event)
-            <div>             
-                <x-event-component :event="$event" />        
-            </div>
-        @endforeach
-    </div>
-
-    <div class="contenedor-pagination-menu">
-        {{ $events->links()}} 
-    </div>
+        <div class="contenedor-pagination-menu">
+            {{ $events->links()}} 
+        </div>
+    @else
+        <div class="eventos-no-encontrados">
+            <p>No se han encotrado eventos</p>
+        </div>    
+    @endif
 
 </div>
 <script src="{{ asset('js/home.js') }}"></script>
