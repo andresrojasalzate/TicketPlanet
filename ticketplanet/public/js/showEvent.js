00bee29a479a4c208e3eaad99599e208803b3d61
Salma <spicazo@alumnat.copernic.cat>
2024-01-22 21:17:30 +0100

    Añadida funcionalidad mostrar la imagen del evento cuando muestras la información de éste

--- ticketplanet/public/js/showEvent.js
+++ ticketplanet/public/js/showEvent.js
@@ -1,13 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const dropdownSesiones = document.getElementById('dropdownSesiones');
-    const sessionTimes = document.querySelectorAll('.session-time');
 
-    dropdownSesiones.addEventListener('change', function () {
-        const selectedDate = dropdownSesiones.options[dropdownSesiones.selectedIndex].getAttribute('data-date');
+const dropdownSesiones = document.getElementById('dropdownSesiones');
+const sessionTimes = document.querySelectorAll('.session-time');
 
-        sessionTimes.forEach(sessionTime => {
-            const sessionDate = sessionTime.getAttribute('data-date');
-            sessionTime.style.display = (sessionDate === selectedDate || selectedDate === null) ? 'block' : 'none';
-        });
+dropdownSesiones.addEventListener('change', function () {
+    const selectedDate = dropdownSesiones.options[dropdownSesiones.selectedIndex].getAttribute('data-date');
+
+    sessionTimes.forEach(sessionTime => {
+        const sessionDate = sessionTime.getAttribute('data-date');
+        sessionTime.style.display = (sessionDate === selectedDate || selectedDate === null) ? 'block' : 'none';
     });
 });
