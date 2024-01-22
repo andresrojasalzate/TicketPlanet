f31260176617219b4c620a995e3ca02e54cd477a
Andres <arojas@alumnat.copernic.cat>
2024-01-22 21:17:52 +0100

    factorys y seeders hechos

--- ticketplanet/database/factories/TicketSessionFactory.php
+++ ticketplanet/database/factories/TicketSessionFactory.php
@@ -3,12 +3,12 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\Session;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Ticket>
  */
-class TicketFactory extends Factory
+class TicketSessionFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,14 +17,11 @@ class TicketFactory extends Factory
      */
     public function definition(): array
     {
-        $sesion = Session::inRandomOrder()->first();
-        
         return [
            'name' => fake()->name(),
            'quantity' => mt_rand(0, 200),
            'price' => fake()->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 1000),
            'nominal' => fake()->randomElement([true, false]), 
-           'session_id' => $sesion->id
         ];
     }
 }
