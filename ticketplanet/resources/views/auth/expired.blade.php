fd495f034bbae1373f64ea7c5809582c14bb23d8
Adrián Bernabeu Malia <abernabeu>
2024-01-23 17:39:40 +0100

    Validacion acabada y SASS al proyecto

--- ticketplanet/resources/views/auth/expired.blade.php
+++ ticketplanet/resources/views/auth/expired.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+<link rel="stylesheet" href="{{ asset('css/styleSASS.css') }}">
 <div class="card-expired">
     <h1>Link Expirado</h1>
     <p>Lo sentimos, el enlace de restablecimiento de contraseña ha caducado. Por favor, <a href="{{ route('password.request') }}">solicita un nuevo enlace</a>.</p>
