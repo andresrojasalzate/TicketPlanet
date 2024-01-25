e3e43e84bacbf4ed2acaed5f332946b4450b8ec2
Salma <spicazo@alumnat.copernic.cat>
2024-01-25 20:20:10 +0100

    Añadidos estilos v.movil pantalla home promotor

--- ticketplanet/resources/views/links/homePromotors.blade.php
+++ ticketplanet/resources/views/links/homePromotors.blade.php
@@ -3,16 +3,19 @@
 @section('title', 'Home Promotor')
 
 @section('content')
-    <div class="btnCrear">
-        <a href="{{ route('links.crearEvento') }}">Crear evento</a>
+    <div class="btnHomePromotor">
+        <div class="btnCrearEvent">
+            <a href="{{ route('links.crearEvento') }}">Crear evento</a>
+        </div>
+
+        <div class="btnAdministrarEvent">
+            <a href="{{ route('links.administrarEvents') }}">Administrar evento</a>
+        </div>
+
+        <div class="btnListarSesionEvent">
+            <a href="{{ route('links.crearEvento') }}">Crear sesiones</a>
+        </div>
     </div>
 
-    <div class="btnAdministrarEvento">
-        <a href="{{ route('links.administrarEvents') }}">Administrar evento</a>
-    </div>
-
-    <div class="btnListarSesionEvento">
-        <a href="{{ route('links.crearEvento') }}">Crear sesiones</a>
-    </div>
 
 @endsection
