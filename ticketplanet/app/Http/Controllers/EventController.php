3125209b09debe4e737b0f2f59ea8c06faebf732
Andres <arojas@alumnat.copernic.cat>
2024-01-27 19:27:37 +0100

    comentarios y loggs en EventController

--- ticketplanet/app/Http/Controllers/EventController.php
+++ ticketplanet/app/Http/Controllers/EventController.php
@@ -9,49 +9,81 @@ use Illuminate\Support\Facades\Log;
 
 class EventController extends Controller
 {
-    public function index() {   
-        return view('events.index', ['events' => Event::with('sessions')->paginate(env('PAGINATION_LIMIT'))]);
-    }
-
+    /**
+     * Procesa la búsqueda de eventos según los parámetros proporcionados en la solicitud.
+     *
+     * @param  \Illuminate\Http\Request  $request La solicitud HTTP recibida.
+     * @return \Illuminate\View\View La vista que muestra los resultados de la búsqueda de eventos.
+     */
     public function search(Request $request){
        
+        Log::info("Asignamos los inputs 'busqueda' y 'category' a variables");
         $busqueda = $request->input('busqueda');
         $categoria =  $request->input('category');
 
+        Log::info("Guardamos los valores de 'busqueda' y 'category'");
         session(['busqueda' => $busqueda]);
         session(['category' => $categoria]);
 
+        Log::info("Devolvemos la vista con los eventos encontrados y las categorias");
         return view('events.index')->with([
         'events' => Event::eventosBuscados($busqueda, $categoria),
         'categories' => Category::all()
        ]);
     }
 
+    /**
+     * Maneja la búsqueda de eventos utilizando los parámetros almacenados en la sesión.
+     * 
+     *
+     * @return \Illuminate\View\View La vista que muestra los resultados de la búsqueda de eventos.
+     */
     public function searchGet(){
-       
+
+        Log::info("Recuperamos de sesion 'busqueda' y 'category'");
         $busqueda = session('busqueda');
         $categoria = session('category');
 
+        Log::info("Devolvemos la vista con los eventos encontrados y las categorias");
         return view('events.index')->with([
         'events' => Event::eventosBuscados($busqueda, $categoria),
         'categories' => Category::all()
        ]);
     }
 
+
+    /**
+     * Maneja la solicitud para filtrar eventos por categoría.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\View\View
+     */
     public function category(Request $request){
+
+        Log::info("Asignamos los inputs 'category' a variables");
         $categoria = $request->input('category');
-    
+        
+        Log::info("Guardamos los valores de 'busqueda' y 'category'");
         session(['categoria' => $categoria]);
         
+        Log::info("Devolvemos la vista con los eventos encontrados y las categorias");
         return view('events.index')->with([
             'events' => Event::where('category_id', $categoria)->with('sessions')->paginate(env('PAGINATION_LIMIT')),
             'categories' => Category::all()
         ]);
     }
 
+    /**
+     * Maneja la solicitud GET para mostrar eventos filtrados por categoría.
+     *
+     * @return \Illuminate\View\View
+     */
     public function categoryGet(){
+
+        Log::info("Recuperamos de sesion 'categoria'");
         $categoria = session('categoria');
         
+        Log::info("Devolvemos la vista con los eventos encontrados y las categorias");
         return view('events.index')->with([
             'events' => Event::where('category_id', $categoria)->with('sessions')->paginate(env('PAGINATION_LIMIT')),
             'categories' => Category::all()
