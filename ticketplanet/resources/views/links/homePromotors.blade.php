2cba16ae2276b1634c0bd253d378f68e0a5896d1
Salma <spicazo@alumnat.copernic.cat>
2024-02-05 16:26:47 +0100

    Actualizados estilos botons de home promotor

--- ticketplanet/resources/views/links/homePromotors.blade.php
+++ ticketplanet/resources/views/links/homePromotors.blade.php
@@ -8,20 +8,6 @@
         <button class="btnCrearEvent" onclick="window.location='{{ route('links.administrarEvents') }}'">Administrar evento</button>
         <button class="btnCrearEvent" onclick="window.location='{{ route('links.sessionEvents') }}'">Crear sesiones</button>
         <button class="btnCrearEvent" onclick="window.location='{{ route('sessions.promotor') }}'">Administrar sesiones</button>
-        <!--<div class="btnCrearEvent">
-            <a href="{{ route('links.crearEvento') }}">Crear evento</a>
-        </div>
-
-        <div class="btnAdministrarEvent">
-            <a href="{{ route('links.administrarEvents') }}">Administrar evento</a>
-        </div>
-
-        <div class="btnListarSesionEvent">
-            <a href="{{ route('links.sessionEvents') }}">Crear sesiones</a>
-        </div>
-        <div class="btnCrearEvent">
-            <a href="{{ route('sessions.promotor') }}">Administrar sesiones</a>
-        </div>-->
     </div>
 
 
