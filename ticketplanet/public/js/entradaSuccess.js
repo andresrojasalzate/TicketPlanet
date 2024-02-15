f8991a4f8289bcbedcfda0239ff75162bbce50c5
Adrián Bernabeu Malia <abernabeu@alumnat.copernic.cat>
2024-02-15 15:27:30 +0000

    Mejoramos diseño crear evento, mejoramos test funcionales de evento(crear...

---
+++ ticketplanet/public/js/entradaSuccess.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let compraExito = document.querySelector('.compraExito');
+  let compraFallido = document.querySelector('.compraFallido');
+  let botonCerrarExito = document.querySelector('.cerrarFeedbackSuccess');
+  let botonCerrar = document.querySelector('.cerrarFeedbackFallido');
+  
+  botonCerrarExito.addEventListener('click',function(){
+    compraExito.style.display = 'none';
+  });
+  
+  // setTimeout(function() {
+  //   compraExito.style.display = 'none';
+  // }, 3000);
+  });
