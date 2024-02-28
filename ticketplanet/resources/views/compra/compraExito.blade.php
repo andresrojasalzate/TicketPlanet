e5774947243294733be50070f32e6c553bc4e918
Adrián Bernabeu Malia <abernabeu>
2024-02-28 15:40:53 +0100

    Mejorados estilos de crear evento, entradas y botones

--- ticketplanet/resources/views/compra/compraExito.blade.php
+++ ticketplanet/resources/views/compra/compraExito.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app')
 @section('title', 'Entrada')
 @section('content')
-<div class="compraExito">
-  La compra de la entrada se ha realizado con exito
-  <a href={{route('home')}}><button type="button" class="cerrarFeedbackSuccess">
-    <span aria-hidden="true">&times;</span>
-    </button></a>
+<div class="contenedorcompraFeedback">
+  <div class="compraExito">
+    La compra de la entrada se ha realizado con exito
+    <a href={{route('home')}}><button type="button" class="cerrarFeedbackSuccess">
+      <span aria-hidden="true">&times;</span>
+      </button></a>
+  </div>
 </div>
+
 <script src="{{ asset('js/entradaSuccess.js') }}"></script>
 @endsection
 
